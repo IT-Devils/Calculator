d00a94da4c208b61e4e1db264e62fdbd7ac8f8c4
WhiS <contact@it-devils.de>
2024-05-08 22:41:53 +0200

    nicht funktional komplette neustrukturierung

--- main.py
+++ main.py
@@ -4,21 +4,35 @@ print("Welcome\n"
       "Numbers can also be written like this: 123.00\n"
       "Valid Operators: +, -, *, /.\n")
 
+last_result = 0
+another_math_task = True
+use_last_result = False
+
+
+def another_math_task():
+    user_input_c = input("You want calculate another Math task? Enter yes or no: ")
+
+    if user_input_c == "yes":
+        return True
+    else:
+        return False
+
 
 def re_use():
     user_input_c = input("You want use the result? Enter yes or no: ")
-    return user_input_c
+
+    if user_input_c == "yes":
+        return True
+    else:
+        return False
 
 
-while True:
-    user_input_a = int(input("Enter the first Number: "))
-    operator = input("Enter a Operator: ")
-    user_input_b = int(input("Enter the second Number: "))
+def calculator(user_input_a, operator, user_input_b):
     last_result = 0
-
     if operator == "+":
         result = user_input_a + user_input_b
         print(result)
+        return result
         last_result = result
     elif operator == "-":
         result = user_input_a - user_input_b
@@ -32,9 +46,12 @@ while True:
         print("Invalid Operator")
         print("Valid Operator are  +, -, *, /")
 
-    user_input_c = re_use()
 
-    if user_input_c == "yes":
-        print(last_result)
+while another_math_task == True:
+    if use_last_result == False:
+        user_input_a = int(input("Enter the first Number: "))
     else:
-        continue
+        user_input_a = last_result
+
+    operator = input("Enter a Operator: ")
+    user_input_b = int(input("Enter the second Number: "))
